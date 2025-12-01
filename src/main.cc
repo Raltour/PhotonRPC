0ad00e317c5868b32b912af71ecec2596a9a73ce
Raltour <15610159165lmz@gmail.com>
2025-12-01 19:00:06 +0800

    fix(acceptor): 解决上次提交中的错误

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,4 @@
 #include "TinyRPC/common/console_logger.h"
-#include "TinyRPC/common/config.h"
 #include "TinyRPC/net/tcp_server.h"
 #include <iostream>
 
@@ -7,9 +6,7 @@ int main() {
   std::cout << "Hello, World!" << std::endl;
   LOG_INFO("Start Server");
 
-  Config& configer = Config::GetInstance();
-
-  TcpServer server(configer.server_host_.c_str(), configer.server_port_);
+  TcpServer server;
 
   return 0;
 }

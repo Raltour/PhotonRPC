05ae76cc6fd508cb2aed3e72c53b9bab84f6deec
Raltour <15610159165lmz@gmail.com>
2025-12-02 16:51:50 +0800

    fix(tcp_connection): 修改TcpConnection的传参构造方式

--- include/TinyRPC/net/tcp_connection.h
+++ include/TinyRPC/net/tcp_connection.h
@@ -5,22 +5,31 @@
 
 class TcpConnection {
 public:
-  TcpConnection(int connect_fd);
+  TcpConnection(int connect_fd, std::function<void()> read_callback,
+                std::function<void()> write_callback,
+                std::function<void(Channel*)> add_connection_callback);
 
-  void set_add_connection_callback(std::function<void(Channel*)> callback);
+  TcpConnection() = delete;
 
-  void set_read_task_callback(std::function<void()> callback);
+  // void set_add_connection_callback(std::function<void(Channel*)> callback);
 
-  void set_write_task_callback(std::function<void()> callback);
+  // void set_read_task_callback(std::function<void()> callback);
+
+  // void set_write_task_callback(std::function<void()> callback);
+
 
 private:
   Channel channel_;
 
+  void set_work(std::function<void()> read_callback,
+                std::function<void()> write_callback,
+                std::function<void(Channel*)> add_connection_callback);
+
   std::function<void(Channel*)> add_connection_callback_;
 
-  std::function<void()> read_task_callback_;
+  // std::function<void()> read_task_callback_;
 
-  std::function<void()> write_task_callback_;
+  // std::function<void()> write_task_callback_;
 };
 
 
--- src/net/tcp_server.cc
+++ src/net/tcp_server.cc
@@ -2,33 +2,57 @@
 
 #include "TinyRPC/common/console_logger.h"
 
-TcpServer::TcpServer()
-  : acceptor_(Acceptor(
-      [this](Channel* channel) {
-        LOG_DEBUG("Acceptor called listen_callback");
-        event_loop_.AddChannel(channel);
-      }
-      ,
-      [this](int connect_fd) {
-        fd_connection_map_.insert({
-            connect_fd,
-            std::make_unique<TcpConnection>(connect_fd)});
+// TcpServer::TcpServer()
+//   : acceptor_(Acceptor(
+//       [this](Channel* channel) {
+//         LOG_DEBUG("Acceptor called listen_callback");
+//         event_loop_.AddChannel(channel);
+//       }
+//       ,
+//       [this](int connect_fd) {
+//         fd_connection_map_.insert({
+//             connect_fd,
+//             std::make_unique<TcpConnection>(connect_fd)});
+//
+//         LOG_DEBUG(
+//             "TcpServer: New TcpConnection with fd: " + std::to_string(connect_fd
+//             ));
+//       })) {
+//   // acceptor_.set_start_listen_callback();
+//
+//   // acceptor_.set_new_connection_callback([this](int connect_fd) {
+//   //   fd_connection_map_.insert({
+//   //       connect_fd,
+//   //       std::make_unique<TcpConnection>(connect_fd)});
+//   //
+//   //   LOG_DEBUG(
+//   //       "TcpServer: New TcpConnection with fd: " + std::to_string(connect_fd));
+//   // });
+// }
 
-        LOG_DEBUG(
-            "TcpServer: New TcpConnection with fd: " + std::to_string(connect_fd
-            ));
-      })) {
-  // acceptor_.set_start_listen_callback();
+TcpServer::TcpServer(std::function<void()> reactor_read,
+                     std::function<void()> reactor_write)
+  : reactor_read_(reactor_read), reactor_write_(reactor_write), acceptor_(
+        Acceptor(
+            [this](Channel* channel) {
+              LOG_DEBUG("Acceptor called listen_callback");
+              event_loop_.AddChannel(channel);
+            }
+            ,
+            [this, reactor_read, reactor_write](int connect_fd) {
+              fd_connection_map_.insert({
+                  connect_fd,
+                  std::make_unique<TcpConnection>(
+                      connect_fd, reactor_read, reactor_write,
+                      [this](Channel* channel) {
+                        event_loop_.AddChannel(channel);
+                      })});
 
-  // acceptor_.set_new_connection_callback([this](int connect_fd) {
-  //   fd_connection_map_.insert({
-  //       connect_fd,
-  //       std::make_unique<TcpConnection>(connect_fd)});
-  //
-  //   LOG_DEBUG(
-  //       "TcpServer: New TcpConnection with fd: " + std::to_string(connect_fd));
-  // });
-}
+              LOG_DEBUG(
+                  "TcpServer: New TcpConnection with fd: " + std::to_string(
+                    connect_fd
+                  ));
+            })) {}
 
 void TcpServer::RunLoop() {
   event_loop_.Loop();

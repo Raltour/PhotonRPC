33c1f3d3de48d95d0fe50f875dbaec5d62a7e4c0
Raltour <15610159165lmz@gmail.com>
2026-04-04 18:58:56 +0800

    feat(request_handler): move handler request to a new class

---
+++ src/rpc/request_handler.cc
@@ -0,0 +1,78 @@
+#include "request_handler.h"
+
+#include "../common/logger.h"
+
+std::string RpcRequestHandler::HandleRequest(const std::string& request,
+                                             const ServiceMap& service_map) {
+  std::string response;
+  rpc::RpcMessage request_message;
+  request_message.ParseFromString(request);
+
+  LOG_DEBUG("Received request: \n{}", request_message.DebugString());
+
+  if (!CheckRequest(request_message, service_map)) {
+    rpc::RpcMessage response_message;
+    response_message.set_id(request_message.id());
+    response_message.set_type(rpc::RPC_TYPE_ERROR);
+    response_message.set_response("Invalid request");
+    response_message.SerializeToString(&response);
+    return response;
+  }
+
+  auto service = service_map.find(request_message.service_name())->second.get();
+  auto service_desc = service->GetDescriptor();
+  auto method_desc =
+      service_desc->FindMethodByName(request_message.method_name());
+
+  auto method_request = service->GetRequestPrototype(method_desc).New();
+  auto method_response = service->GetResponsePrototype(method_desc).New();
+  method_request->ParseFromString(request_message.request());
+
+  service->CallMethod(method_desc, nullptr, method_request, method_response,
+                      nullptr);
+
+  rpc::RpcMessage response_message;
+  response_message.set_id(request_message.id());
+  response_message.set_type(rpc::RPC_TYPE_RESPONSE);
+  response_message.set_response(method_response->SerializeAsString());
+  response_message.SerializeToString(&response);
+
+  delete method_request;
+  delete method_response;
+
+  LOG_DEBUG("Send response: \n{}", response_message.DebugString());
+  return response;
+}
+
+bool RpcRequestHandler::CheckRequest(const rpc::RpcMessage& request,
+                                     const ServiceMap& service_map) {
+  if (request.type() != rpc::RPC_TYPE_REQUEST) {
+    LOG_ERROR("Invalid request type: {}", static_cast<int>(request.type()));
+    return false;
+  }
+
+  if (request.method_name().empty()) {
+    LOG_ERROR("Empty method name");
+    return false;
+  }
+
+  if (request.service_name().empty()) {
+    LOG_ERROR("Empty service name");
+    return false;
+  }
+
+  if (service_map.find(request.service_name()) == service_map.end()) {
+    LOG_ERROR("Service not found: {}", request.service_name());
+    return false;
+  }
+
+  auto service = service_map.find(request.service_name())->second.get();
+  auto service_desc = service->GetDescriptor();
+  auto method_desc = service_desc->FindMethodByName(request.method_name());
+  if (method_desc == nullptr) {
+    LOG_ERROR("Method not found: {}", request.method_name());
+    return false;
+  }
+
+  return true;
+}
---
+++ src/rpc/request_handler.h
@@ -0,0 +1,25 @@
+#ifndef PHOTONRPC_REQUEST_HANDLER_H
+#define PHOTONRPC_REQUEST_HANDLER_H
+
+#include <google/protobuf/service.h>
+
+#include <map>
+#include <memory>
+#include <string>
+
+#include "photonrpc/rpc_message.pb.h"
+
+class RpcRequestHandler {
+ public:
+  using ServiceMap =
+      std::map<std::string, std::unique_ptr<google::protobuf::Service>>;
+
+  static std::string HandleRequest(const std::string& request,
+                                   const ServiceMap& service_map);
+
+ private:
+  static bool CheckRequest(const rpc::RpcMessage& request,
+                           const ServiceMap& service_map);
+};
+
+#endif  // PHOTONRPC_REQUEST_HANDLER_H
